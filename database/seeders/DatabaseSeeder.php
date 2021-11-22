052c2045742e1c72eb1421652d5f3a812df202f6
Pandu Ananto <59494215+panduananto@users.noreply.github.com>
2021-11-22 18:14:01 +0700

    create recipe: model. controller, migration table, db seeder and change route to use the controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,28 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        DB::table('recipes')->insert([
+            [
+                'title' => 'Roti bakar',
+                'description' => 'Resep roti bakar garing dengan keju dan cokelat yang meleleh.',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'title' => 'Ayam goreng',
+                'description' =>
+                    'Ayam goreng dengan bumbu jahe yang gurih, asin dan bikin ketagihan!',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'title' => 'Telor mata sapi',
+                'description' =>
+                    'Telor goreng mata sapi sederhana dengan pinggiran yang crispy dan kuning telur yang segar.',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\RecipeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/recipe');
 
 Route::prefix('recipe')->name('recipe.')->group(function() {
-    Route::get('/', function() {
-        return view('pages.index');
-    })->name('index');
+    Route::get('/', [RecipeController::class, 'index'])->name('index');
 });
