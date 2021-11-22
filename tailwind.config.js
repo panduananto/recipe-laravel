4183974359453d92d1bbbfc2249750b4aa5ca4d1
Pandu Ananto <59494215+panduananto@users.noreply.github.com>
2021-11-22 22:54:44 +0700

    extend spacing in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
   purge: ['./resources/**/*.blade.php', './resources/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '1/2': '50%',
+        '1/3': '33.333333%',
+        '2/3': '66.666667%',
+        '1/4': '25%',
+        '2/4': '50%',
+        '3/4': '75%',
+      },
+    },
     fontFamily: {
       sans: ['Inter', ...defaultTheme.fontFamily.sans],
       serif: ['IBM Plex Serif', ...defaultTheme.fontFamily.serif],
