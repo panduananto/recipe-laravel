53c139cebb55de495ee19782c0345429507cd5e7
Pandu Ananto <59494215+panduananto@users.noreply.github.com>
2021-11-22 18:42:05 +0700

    read recipes data to recipe.index page

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Recipe;
+
 class RecipeController extends Controller
 {
     public function index()
     {
-        return view('pages.index');
+        return view('pages.index')->with('recipes', Recipe::all());
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.app')
 @section('content')
   <div class="max-w-screen-xl px-8 mx-auto mt-14">
-    
+    <div class="grid grid-cols-6 gap-x-2">
+      <div class="col-span-6 space-y-2">
+          @if(count($recipes) == 0)
+            <div class="py-4 text-center border border-gray-300 border-solid rounded-lg">We do not have any recipes to display right now.</div>
+          @else
+            <div class="grid grid-cols-12 gap-4">
+              @foreach($recipes as $recipe)
+              <div class="flex flex-row col-span-12 overflow-hidden border border-gray-300 border-solid rounded-lg shadow-sm sm:flex-col sm:col-span-6 md:col-span-4 lg:col-span-3">
+                <div class="px-4 py-3 overflow-hidden sm:p-4">
+                  <a href="#" class="block text-base font-extrabold text-gray-900 truncate hover:underline">{{$recipe->title}}</a>
+                  <p class="mb-4 text-sm text-gray-900 truncate">{{$recipe->description}}</p>
+                </div>
+              </div>
+              @endforeach
+            </div>
+          @endif
+      </div>
+    </div>
   </div>
 @endsection
