5f2c2c328c642d62559c0fd1d28c26947cece465
Pandu Ananto <59494215+panduananto@users.noreply.github.com>
2021-11-25 17:27:15 +0700

    install uuidv4

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,14 @@
 import Alpine from 'alpinejs';
+import { v4 as uuidv4 } from 'uuid';
 
 require('./bootstrap');
 
 window.Alpine = Alpine;
+
+Alpine.data('uuid', () => ({
+  generateUuid() {
+    return uuidv4();
+  },
+}));
 
 Alpine.start();
