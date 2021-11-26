659634b6bc633f090f8faee0b5eaf4a1cdb72681
Pandu Ananto <59494215+panduananto@users.noreply.github.com>
2021-11-26 11:17:40 +0700

    install alpinejs/persist

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
 import Alpine from 'alpinejs';
+import persist from '@alpinejs/persist';
 import { v4 as uuidv4 } from 'uuid';
 
 require('./bootstrap');
 
 window.Alpine = Alpine;
+
+Alpine.plugin(persist);
 
 Alpine.data('uuid', () => ({
   generateUuid() {
