e0923cd3c7a590124864ff4e4c96ad018217c00a
Pandu Ananto <59494215+panduananto@users.noreply.github.com>
2021-11-29 21:52:02 +0700

    adjustment on comment section layout

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -28,37 +28,44 @@
       </div>
       <div class="pt-8">
         <h3 class="mb-8 text-2xl font-bold text-gray-900">Comments ({{count($comments)}})</h3>
-        <form action="{{route('recipe.comment.store', ['id' => $recipe->id])}}" method="POST">
-          @csrf
-          <div class="flex mb-16 gap-x-4">
-            @auth
-              <div class="flex items-center justify-center w-10 h-10 text-xl font-medium text-white bg-blue-600 rounded-full">
-                {{Str::substr(auth()->user()->name, 0, 1)}}
-              </div>
-              <div class="flex-1">
-                <label for="body_comment"></label>
-                <textarea name="body_comment" rows="4" placeholder="Add a comment..." class="w-full border border-gray-300 rounded-lg"></textarea>
-                <button type="submit" class="w-full px-4 py-2 text-lg text-white bg-blue-600 rounded-lg hover:bg-blue-700">Post</button>
-              </div>
-            @else
-              <div>You need to <a href="{{route('login.index')}}" class="font-medium text-blue-600 hover:underline">log in</a> to post a comment</div>
-            @endauth
-          </div>
-        </form>
-        <ul class="space-y-10">
-          @foreach($comments as $comment)
-          <li class="flex gap-x-4">
-            <div class="flex items-center justify-center w-10 h-10 text-xl font-medium text-white bg-blue-600 rounded-full">
-              {{Str::substr($comment->user->name, 0, 1)}}
+        <div class="space-y-8">
+          @if(count($comments) == 0)
+            <div class="p-4 mb-8 text-center bg-gray-200 rounded-lg ">There is no comments on this recipe.</div>
+          @endif
+          <form action="{{route('recipe.comment.store', ['id' => $recipe->id])}}" method="POST">
+            @csrf
+            <div class="flex gap-x-4">
+              @auth
+                <div class="flex items-center justify-center w-10 h-10 text-xl font-medium text-white bg-blue-600 rounded-full">
+                  {{Str::substr(auth()->user()->name, 0, 1)}}
+                </div>
+                <div class="flex-1">
+                  <label for="body_comment"></label>
+                  <textarea name="body_comment" rows="4" placeholder="Add a comment..." class="w-full mb-2 border border-gray-300 rounded-lg"></textarea>
+                  <button type="submit" class="w-full px-4 py-2 text-lg text-white bg-blue-600 rounded-lg hover:bg-blue-700">Post</button>
+                </div>
+              @else
+                <div>You need to <a href="{{route('login.index')}}" class="font-medium text-blue-600 hover:underline">log in</a> to post a comment</div>
+              @endauth
             </div>
-            <div class="flex-1">
-              <p class="text-lg font-semibold text-gray-900">{{$comment->user->name}}</p>
-              <p class="text-gray-700">{{$comment->body}}</p>
-              <p class="mt-4 text-sm text-gray-400 font-base">{{$comment->created_at->diffForHumans()}}</p>
-            </div>
-          </li>
-          @endforeach
-        </ul>
+          </form>
+          @if(count($comments) != 0)
+            <ul class="space-y-10">
+              @foreach($comments as $comment)
+              <li class="flex gap-x-4">
+                <div class="flex items-center justify-center w-10 h-10 text-xl font-medium text-white bg-blue-600 rounded-full">
+                  {{Str::substr($comment->user->name, 0, 1)}}
+                </div>
+                <div class="flex-1">
+                  <p class="text-lg font-semibold text-gray-900">{{$comment->user->name}}</p>
+                  <p class="text-gray-700">{{$comment->body}}</p>
+                  <p class="mt-4 text-sm text-gray-400 font-base">{{$comment->created_at->diffForHumans()}}</p>
+                </div>
+              </li>
+              @endforeach
+            </ul>
+          @endif
+        </div>
       </div>
     </div>
   </div>
